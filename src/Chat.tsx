6d20971894f5833b1e8faca1757193ab0243fd3b
timmatsam <timmatsam@gmail.com>
2023-04-19 20:15:35 -0400

    feat: add auto scrolling

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const EXPRESS_SERVER = import.meta.env.VITE_EXPRESS_SERVER;
 const Chat = () => {
   const [messages, setMessages] = useState<{role: string, content: string}[]>([]);
   const [newMessage, setNewMessage] = useState<{role: string, content: string}>({ role: "", content: "" });
+  const [currInputValue, setCurrInputValue] = useState<string>("");
+  const dummyRef: any = useRef(null);
+
   const handleMessageSubmit = (e:any) => {
     e.preventDefault();
     const userMessage = {
@@ -12,6 +15,7 @@ const Chat = () => {
     };
     setNewMessage(userMessage);
     setMessages([...messages, userMessage]);
+    setCurrInputValue("");
   };
   useEffect(() => {
     fetch(`${EXPRESS_SERVER}/openai/messages`, {
@@ -24,11 +28,15 @@ const Chat = () => {
       .then((res) => res.json())
       .then(message => {
         setMessages([...messages, message]);
+      }).then(() => {
       });
   }, [newMessage]);
+  useEffect(() => {
+    dummyRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
   return (
-    <div className='flex-col justify-center w-screen'>
-        <div className="flex flex-col overscroll-none overflow-auto h-72 my-1">
+      <div className='flex-col justify-center w-screen'>
+        <div className="flex flex-col overflow-auto h-72 my-1">
           {messages.map((message, index) => (
             <div key={index} className={`p-2 ${message.role === "user" ? "self-end" : "self-start"}`}>
               <div className={`rounded-lg p-2 ${message.role === "user" ? "bg-gray-200 text-black" : "bg-blue-600 text-white"}`}>
@@ -36,13 +44,13 @@ const Chat = () => {
               </div>
             </div>
           ))}
+        <div ref={dummyRef}/>
         </div>
-
         <form onSubmit={handleMessageSubmit} className="w-full">
-          <input type='text' className='border-solid border-4 border-sky-400  w-full' placeholder="send a chat as if you were texting your friend (but in japanese)"/>
+          <input value={currInputValue} onChange={(e) => setCurrInputValue(e.target.value)}type='text' className='border-solid border-4 border-sky-400  w-full' placeholder="send a chat as if you were texting your friend (but in japanese)"/>
         </form>
     </div>
   );
 };
-
+// TODO: change any types
 export default Chat;
